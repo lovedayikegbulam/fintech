c54f52098e530db083a0e62d526988b22c8cde6c
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-03-03 16:39:12 +0100

    add middleware to user schema for password hashing before saving

--- config/config.js
+++ config/config.js
@@ -9,6 +9,7 @@ const config =  {
     PORT: process.env.PORT,
     MONGODB_URI: process.env.MONGODB_URI,
     LOCAL_HOST: process.env.LOCAL_HOST,
+    SALT_ROUND: process.env.SALT_ROUND
 }
 
 export default config;
--- models/user.schema.js
+++ models/user.schema.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import CONFIG from "../config/config.js";
+import bcrypt from "bcrypt";
 
 // Schema
 const userSchema = mongoose.Schema({
@@ -12,13 +14,29 @@ const userSchema = mongoose.Schema({
     required: true,
   },
   password: {
-    type: String, 
-    required: true 
-  }, 
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
+});
+
+// Mongoose middleware to hash the password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(14);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 // Model
